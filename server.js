630a1cc607d17ec4d1f17560f022bf18aafd4904
tanuj <tanuj4321@gmail.com>
2022-05-06 11:25:08 +0530

    your commit message

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = require('./routes/routes.js')(app, fs);
-
-const server = app.listen(3001, () => {
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
   console.log('listening on port %s...', server.address().port);
 });
